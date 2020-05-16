b718fbf3f5b6fc3c6f247310380f652008948f90
Nick Kapetanas <nkapetanas@outlook.com>
2020-05-16 20:59:53 +0300

    simple page rank impl fixes

--- src/SimplePageRankImpl.py
+++ src/SimplePageRankImpl.py
@@ -1,8 +1,8 @@
 from src.graph import Graph
 from src.node import Node
 
-#WEB_GOOGLE_TEXT_FILE = "../data/web-Google.txt"
-WEB_GOOGLE_TEXT_FILE = "../data/webSample.txt"
+# WEB_GOOGLE_TEXT_FILE = "../data/web-Google.txt"
+WEB_GOOGLE_TEXT_FILE = "../data/webSample2.txt"
 
 
 def read_file(file):
@@ -16,18 +16,22 @@ def read_file(file):
     return text_lines
 
 
-def page_rank(num_of_webpages, graph, num_iters=10):
+def page_rank(graph, num_iters=10):
     # initialize a dictionary with node value and a unit of importance
     pages_with_score = {node: 1 for node in graph.get_nodes()}
 
     for _ in range(num_iters):
+        old_pages_with_score = dict(pages_with_score)
+
         for page, score in pages_with_score.items():
             connections = graph.get_connections_from_specific_node(page)
             new_score = 0
             for connection in connections:
-                new_score += pages_with_score[connection] / graph.get_number_of_connections_from_specific_node(connection)
+                new_score += old_pages_with_score[connection] / graph.get_number_of_connections_from_specific_node(
+                    connection)
 
             pages_with_score[page] = new_score
+
 
 def initiliaze_graph(text):
     graph = Graph()
@@ -39,9 +43,8 @@ def initiliaze_graph(text):
 
     return graph
 
+
 text = read_file(WEB_GOOGLE_TEXT_FILE)
 graph = initiliaze_graph(text)
 
 page_rank(1, graph)
-
-
