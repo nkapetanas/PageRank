994421bc7e9f36ecfa6ae2af5ceae7992f529fa3
Nick Kapetanas <nkapetanas@outlook.com>
2020-05-17 20:15:28 +0300

    added histogram impl and time execution metrics

--- src/SimplePageRankImpl.py
+++ src/SimplePageRankImpl.py
@@ -1,9 +1,13 @@
 from src.graph import Graph
 from src.node import Node
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 # WEB_GOOGLE_TEXT_FILE = "../data/web-Google.txt"
-WEB_GOOGLE_TEXT_FILE = "../data/webSample2.txt"
+#WEB_GOOGLE_TEXT_FILE = "../data/webSample2.txt"
+WEB_GOOGLE_TEXT_FILE = "../data/webSample.txt"
+TIME_PRINT = "--- %s seconds ---"
 
 
 def read_file(file):
@@ -17,7 +21,7 @@ def read_file(file):
     return text_lines
 
 
-def page_rank(graph, num_iters=10):
+def page_rank(graph, num_iters):
     # initialize a dictionary with node value and a unit of importance
     pages_with_score = {node: 1 for node in graph.get_nodes()}
 
@@ -35,7 +39,7 @@ def page_rank(graph, num_iters=10):
     return pages_with_score
 
 
-def page_rank_imporved(graph, a, num_iters=10):
+def page_rank_imporved(graph, a, num_iters):
     # initialize a dictionary with node value and a unit of importance
     pages_with_score = {node: 1 for node in graph.get_nodes()}
 
@@ -66,25 +70,69 @@ def initiliaze_graph(text):
     return graph
 
 
-def get_n_elements_dict(n, reversed_list):
-
-    if(reversed_list):
-        for key in list(reversed(list(rankings_sorted)))[0:3]:
-            print("key {}, value {} ".format(key, rankings_sorted[key]))
+def get_n_elements_dict(n, reversed_list, sorted_dict):
+    if reversed_list:
+        for key in list(reversed(list(sorted_dict)))[0:3]:
+            print("key {}, value {} ".format(key, sorted_dict[key]))
         return True
 
-    for key in list(rankings_sorted)[0:n]:
-        print("key {}, value {} ".format(key, rankings_sorted[key]))
+    for key in list(sorted_dict)[0:n]:
+        print("key {}, value {} ".format(key, sorted_dict[key]))
 
 
-text = read_file(WEB_GOOGLE_TEXT_FILE)
+def compute_page_rank(a, iter_number):
+    text = read_file(WEB_GOOGLE_TEXT_FILE)
 
-graph = initiliaze_graph(text)
+    graph = initiliaze_graph(text)
 
-rankings = page_rank_imporved(graph, 0.2)
+    rankings = page_rank(graph, iter_number)
 
-rankings_sorted = {k: v for k, v in sorted(rankings.items(), key=lambda item: item[1])}
+    rankings_sorted = {k: v for k, v in sorted(rankings.items(), key=lambda item: item[1])}
 
-get_n_elements_dict(20, False)
-get_n_elements_dict(20, True)
+    get_n_elements_dict(20, False, rankings_sorted)
+    get_n_elements_dict(20, True, rankings_sorted)
 
+
+def compute_page_rank_improved(a, iter_number):
+    text = read_file(WEB_GOOGLE_TEXT_FILE)
+
+    graph = initiliaze_graph(text)
+
+    rankings = page_rank_imporved(graph, a, iter_number)
+
+    rankings_sorted = {k: v for k, v in sorted(rankings.items(), key=lambda item: item[1])}
+
+    get_n_elements_dict(20, False, rankings_sorted)
+    get_n_elements_dict(20, True, rankings_sorted)
+
+
+def create_histogram(x, y):
+    plt.bar(x, y, align='center')
+    plt.xlabel('Pagerank')
+    plt.ylabel('Count')
+    plt.show()
+
+
+start_time = time.time()
+compute_page_rank(0.2, 50)
+print(TIME_PRINT % (time.time() - start_time))
+
+start_time = time.time()
+compute_page_rank(0.2, 100)
+print(TIME_PRINT % (time.time() - start_time))
+
+start_time = time.time()
+compute_page_rank(0.2, 200)
+print(TIME_PRINT % (time.time() - start_time))
+
+start_time = time.time()
+compute_page_rank_improved(0.2, 50)
+print(TIME_PRINT % (time.time() - start_time))
+
+start_time = time.time()
+compute_page_rank_improved(0.2, 100)
+print(TIME_PRINT % (time.time() - start_time))
+
+start_time = time.time()
+compute_page_rank_improved(0.2, 200)
+print(TIME_PRINT % (time.time() - start_time))
